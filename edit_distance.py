c114df615bad11b6d60f7f1aa9cfb474168bbb6d
Jimson Huang <jimsonjy@gmail.com>
2023-02-21 22:23:38 -0800

    Started edit_distance

--- edit_distance.py
+++ edit_distance.py
@@ -1,6 +1,14 @@
+import numpy as np
+
+
 def calc_min_edit_dist(source, target):
+    med_mat = np.zeros((len(source) + 1, len(target) + 1))
+    print(med_mat)
     pass
 
 
 def align(source, target):
     pass
+
+
+calc_min_edit_dist("intention", "execution")
